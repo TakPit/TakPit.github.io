c679285d67ccede521d554e8579083cfb66e88e1
Pietro Maran <pietromaran03@gmail.com>
2025-04-02 17:53:23 -0600

    wa11

---
+++ wa/wa11/main.js
@@ -0,0 +1,37 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const images = ['lago.png', `loco.png`, `zen.png`, `torino.png`, `special_guests.png`];
+/* Declaring the alternative text for each image file */
+const alts = {'lago.png': 'lago', 'loco.png': 'loco', 'zen.png': 'zen', 'torino.png': 'torino', 'special_guests.png': 'speical_guests'}
+
+/* Looping through images */
+for (const image of images) {
+    const img = document.createElement('img');
+    img.setAttribute('src', `images/${image}`);
+    img.setAttribute('alt', image.split('.')[0]);
+    thumbBar.appendChild(img);
+}
+thumbBar.addEventListener('click', function (event) {
+    clickedImg = event.target;
+      displayedImage.src = clickedImg.src;
+      displayedImage.alt = clickedImg.alt;
+  });
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    if (btn.getAttribute('class') === "dark") {
+        btn.setAttribute('class', "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    } else {
+        btn.setAttribute('class', "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+});
