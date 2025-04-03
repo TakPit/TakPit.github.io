778144136724374ba959c624986539713403f802
Pietro Maran <pietromaran03@gmail.com>
2025-04-02 18:58:57 -0600

    wa11

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -4,22 +4,25 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
+const altText = document.querySelector('.alt-text');
+
 /* Declaring the array of image filenames */
 const images = ['lago.png', `loco.png`, `zen.png`, `torino.png`, `special_guests.png`];
 /* Declaring the alternative text for each image file */
-const alts = {'lago.png': 'lago', 'loco.png': 'loco', 'zen.png': 'zen', 'torino.png': 'torino', 'special_guests.png': 'speical_guests'}
+const alts = {'lago.png': 'Lago', 'loco.png': 'LoCo', 'zen.png': 'Zen Hooked', 'torino.png': 'Torino', 'special_guests.png': 'Special guests'}
 
 /* Looping through images */
 for (const image of images) {
     const img = document.createElement('img');
     img.setAttribute('src', `images/${image}`);
-    img.setAttribute('alt', image.split('.')[0]);
+    img.setAttribute('alt', alts[image]);
     thumbBar.appendChild(img);
 }
 thumbBar.addEventListener('click', function (event) {
     clickedImg = event.target;
       displayedImage.src = clickedImg.src;
       displayedImage.alt = clickedImg.alt;
+      altText.textContent = clickedImg.alt;
   });
 
 /* Wiring up the Darken/Lighten button */
